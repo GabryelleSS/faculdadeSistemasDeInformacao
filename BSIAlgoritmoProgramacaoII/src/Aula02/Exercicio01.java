84a4375fef340c04a17c375c7194e1c5d8a2dedd
Gabryelle SS <gabryelle-gaby@hotmail.com>
2019-08-14 21:10:19 -0300

    Adding exercicios

---
+++ BSIAlgoritmoProgramacaoII/src/Aula02/Exercicio01.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Aula02;
+
+/**
+ *
+ * @author gabryelle.ssantos
+ */
+public class Exercicio01 {
+    public static void main(String[] args) {
+        int number = 65000;
+        int conversao = (short)number;
+        
+        System.out.println(conversao);
+    }
+}
