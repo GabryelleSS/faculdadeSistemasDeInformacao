7401d3366c03387388aea3e5a2836742fb0f64bc
= <=>
2020-03-12 21:53:13 -0300

    feat: instantiating Student object

--- terceiroSemestre/POO/EscolaNatacao/src/escolanatacao/EscolaNatacao.java
+++ terceiroSemestre/POO/EscolaNatacao/src/escolanatacao/EscolaNatacao.java
@@ -1,12 +1,35 @@
 package escolanatacao;
 
 public class EscolaNatacao {
+    
+    // O static torna a variavel como atributo de classe.
+    private static int QUANT_ALUNO = 2;
+    private static Aluno[] novoAluno = new Aluno[QUANT_ALUNO];
+    private static int indiceAluno = 0;
+    
+    static void cadastarNovoAluno() {
+        
+        if (indiceAluno >= novoAluno.length) {
+            System.out.println("Limite excedido!");
+        }
+        else {
+            Aluno aluno = new Aluno();
+            aluno.cadastrarAluno();
+            Aluno aluno2 = aluno;
+            
+            novoAluno[indiceAluno] = aluno;
+            indiceAluno++;
+            
+        }
+        
+    }
 
     public static void main(String[] args) {
         
         Aluno aluno = new Aluno();
         
-        aluno.cadastrarAluno();
+        System.out.println("Cadastre um aluno:");
+        cadastarNovoAluno();
     
     }
     
